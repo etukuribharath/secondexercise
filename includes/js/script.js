51918030f999185133cfebe9653fddbb20238627
Etukuri Bharath <etukuri.bharath123@gmail.com>
2016-07-11 07:47:41 +0530

    Second Assignment

---
+++ includes/js/script.js
@@ -0,0 +1,13 @@
+/*
+My Custom JS
+============
+*/
+// navigation bar scroll effect
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
